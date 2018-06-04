e963425202b36cd36bd2a847468022bfdf56e575
wang0nya <kwangonya@gmail.com>
2018-06-04 20:30:15 +0300

    add firebase

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -14,11 +14,7 @@ export class SignupPage {
   // The account fields for the login form.
   // If you're using the username field with or without email, make
   // sure to add it to the type
-  account: { name: string, email: string, password: string } = {
-    name: 'Test Human',
-    email: 'test@example.com',
-    password: 'test'
-  };
+  account: { email: string, password: string };
 
   // Our translated text strings
   private signupErrorString: string;
