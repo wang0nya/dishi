4bc5da9d5c4cde916bb4754ee24ed2d1921506e3
wang0nya <kwangonya@gmail.com>
2018-06-21 15:17:16 +0300

    improve interface

--- src/pages/analyze/analyze.ts
+++ src/pages/analyze/analyze.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 import { RecipeProvider } from "../../providers";
 import { Api } from "../../providers";
+import { Chart } from 'chart.js';
 
 /**
  * Generated class for the AnalyzePage page.
@@ -16,6 +17,8 @@ import { Api } from "../../providers";
   templateUrl: 'analyze.html',
 })
 export class AnalyzePage {
+  @ViewChild('barCanvas') barCanvas;
+  barChart: any;
   public currentRecipe: any = {};
   data: any;
   ingredients: any;
@@ -37,11 +40,54 @@ export class AnalyzePage {
 
   getData() {
     this.ingredients = encodeURIComponent(this.currentRecipe.ingredients);
-    this.serving = encodeURIComponent(this.currentRecipe.serving);
+    this.serving = encodeURIComponent(this.currentRecipe.serves);
     this.api.get(this.ingredients, this.serving)
       .then(data => {
         this.data = data;
-        console.log('data ==>',this.data);
+        this.barChart = new Chart(this.barCanvas.nativeElement, {
+
+          type: 'horizontalBar',
+          data: {
+            labels: ["Fat", "Carbs", "Fiber", "Protein", "Sugars"],
+            datasets: [{
+              label: 'Total Nutrients (g)',
+              data: [
+                this.data.totalNutrients.FAT.quantity,
+                this.data.totalNutrients.CHOCDF.quantity,
+                this.data.totalNutrients.FIBTG.quantity,
+                this.data.totalNutrients.PROCNT.quantity,
+                this.data.totalNutrients.SUGAR.quantity
+              ],
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)'
+                // 'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)'
+                // 'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+            }]
+          },
+          options: {
+            scales: {
+              yAxes: [{
+                ticks: {
+                  beginAtZero:true
+                }
+              }]
+            }
+          }
+
+        });
       });
   }
 
